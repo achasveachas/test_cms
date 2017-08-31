434046c340e4662d918da954b76f8230541331da
Yechiel Kalmenson <contact@yechiel.me>
2017-08-30 22:50:10 -0400

    S6.  adds functionality for app to remember how the contacts were last sorted

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,4 +1,9 @@
 $(function(){
+
+    var path = window.location.pathname
+    if (!window.location.href.split('?')[1] && (path === '/' || path === '/contacts') && window.localStorage.sortBy) {
+        location.href = '/contacts?sort=' + window.localStorage.sortBy
+    }
     
     // Function to validate the format of phone numbers in the new contact form
     $(document).on('click', '.btn-primary', function(e){
@@ -16,6 +21,8 @@ $(function(){
     // Function to sort the list of contacts
     $(document).on('click', 'th', function(e){
         var sortBy = event.target.dataset.sortBy
+        window.localStorage.sortBy = sortBy
         location.href = '/contacts?sort=' + sortBy
     })
+
 })
