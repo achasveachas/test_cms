5c935cc1884ca424ba499fb647d5eb8d29477bf2
Yechiel Kalmenson <contact@yechiel.me>
2017-09-01 16:06:41 -0400

    S8. refactor a method to hash contact's email address for gravadar

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ApplicationRecord
   validates :name, presence: true
   validates :email, uniqueness: {scope: :name}
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  def gravatar_hash
+    Digest::MD5.hexdigest(self.email.strip.downcase)
+  end
 end
