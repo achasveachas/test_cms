3aad6037fa721b9d0f72691974ac30faefc7682a
Yechiel Kalmenson <contact@yechiel.me>
2017-09-02 23:03:51 -0400

    S12. adds function to get and display contact information

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -2,8 +2,8 @@ $(function(){
 
     
     // Function to validate the format of phone numbers in the new contact form
-    $(document).on('click', '.btn-primary', function(e){
-        e.preventDefault()
+    $(document).on('click', '.btn-primary', function(event){
+        event.preventDefault()
 
         var phoneNumber = $('#contact_phone').val()
         if (validatePhone(phoneNumber)) {
@@ -15,14 +15,45 @@ $(function(){
     })
 
     // Function to sort the list of contacts
-    $(document).on('click', 'th', function(e){
+    $(document).on('click', 'th', function(event){
         var sortBy = event.target.dataset.sortBy
         window.localStorage.sortBy = sortBy
         location.href = '/contacts?sort=' + sortBy
     })
 
+    // Function to open the contact card
+    $('td[colspan=5]').find('p').hide();
+    $(document).on('click', 'tr', function(event){
+        var id = event.currentTarget.dataset.id
+        var target = event.target
+
+        if($('#contact-card-' + id).is(':empty')) {
+            getContact(id, target)
+        } else {
+            toggleContact(target)
+        }
+
+
+    })
 })
 
 function validatePhone (phoneNumber) {
     return !!phoneNumber.match(/^(\d{3}-\d{3}-\d{4}|\d{3}-\d{4})$/)
+}
+
+function getContact(id, target){
+    $.get('/contacts/' + id, function(data){
+        
+        $('#contact-card-' + id).html(data)
+
+        $(target).closest("tr").next().find('p').slideToggle();
+    })
+}
+
+function toggleContact(target){
+    if ( $(target).closest("td").attr("colspan") > 1 ) {
+        $(target).slideUp();
+    } else {
+        $(target).closest("tr").next().find('p').slideToggle();
+    } 
 }
