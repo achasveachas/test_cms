10f9b5c50a47d33bb4373e855cdead8d862967de
Yechiel Kalmenson <contact@yechiel.me>
2017-09-03 11:21:55 -0400

    S15. add telephone model and set up one to many relationship with contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
+  has_many :telephones
+  
   validates :name, presence: true
   validates :email, uniqueness: {scope: :name}
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
