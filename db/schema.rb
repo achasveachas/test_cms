ea339a899e5df52d054a10146bb9fba268b9ebf7
Yechiel Kalmenson <contact@yechiel.me>
2017-09-03 00:31:34 -0400

    S14. add a notes column to the contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901013952) do
+ActiveRecord::Schema.define(version: 20170903043045) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170901013952) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "birthday"
+    t.text "notes"
   end
 
 end
