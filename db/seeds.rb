0fd41bdb28e468c8f777784cc466b788fc6f401b
Yechiel Kalmenson <contact@yechiel.me>
2017-09-03 14:47:29 -0400

    update the seed file to account for new phone number format

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 100.times do
     Contact.create name: Faker::Name.name, 
-               email: Faker::Internet.email, 
-               phone: "#{Faker::Number.number(3)}-#{Faker::Number.number(3)}-#{Faker::Number.number(4)}"
+               email: Faker::Internet.email
 end
 
-
+Contact.all.each do |contact|
+    contact.telephones.create number: "#{Faker::Number.number(3)}-#{Faker::Number.number(3)}-#{Faker::Number.number(4)}",
+        location: "Home",
+        default: true
+end
 
