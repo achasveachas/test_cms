baa1778beef5347da87c367fb83a7fe9c1a8d1dd
Yechiel Kalmenson <contact@yechiel.me>
2017-09-03 20:47:21 -0400

    improve rendering of phone numbers in contact card

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,6 @@
 class Contact < ApplicationRecord
   has_many :telephones, :dependent => :destroy
-  accepts_nested_attributes_for :telephones, reject_if: proc { |attr| attr['number'].empty? }
-  
+  accepts_nested_attributes_for :telephones, :reject_if => proc { |attributes| attributes['number'].blank? }
   validates :name, presence: true
   validates :email, uniqueness: {scope: :name}
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
